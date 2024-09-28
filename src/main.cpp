9d633ca77511aea91ceb476c98ff9d91a60f28f2
TillWege <tillwege@gmail.com>
2024-09-28 19:02:23 +0200

    Add Raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include "raylib.h"
 
-int main()
+int main(void)
 {
-	std::cout << "Hello, World!" << std::endl;
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "Trainering");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		auto t = "Hello, Window!";
+		auto a = MeasureText(t, 20);
+		DrawText(t, (screenWidth  - a) / 2, 200, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
+	CloseWindow();
+
 	return 0;
 }
