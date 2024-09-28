d1cd5e17b5b7e77c770c7bf79e1d07779947257d
TillWege <tillwege@gmail.com>
2024-09-28 19:28:36 +0200

    Add ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
 #include "raylib.h"
+#include "imgui.h"
+#include "rlImGui.h"
 
 int main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
 
 	InitWindow(screenWidth, screenHeight, "Trainering");
-
+	rlImGuiSetup(true);
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+		rlImGuiBegin();
 
 		ClearBackground(RAYWHITE);
 
 		auto t = "Hello, Window!";
 		auto a = MeasureText(t, 20);
-		DrawText(t, (screenWidth  - a) / 2, 200, 20, LIGHTGRAY);
+		DrawText(t, (screenWidth  - a) / 2, (screenHeight - 20) / 2, 20, LIGHTGRAY);
 
+		ImGui::ShowDemoWindow();
+		rlImGuiEnd();
 		EndDrawing();
 	}
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
