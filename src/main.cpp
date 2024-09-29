38a8e8ee1b18cdba68194eb833b15fa16d10f089
TillWege <tillwege@gmail.com>
2024-09-29 23:10:56 +0200

    Organize Models, add basic floor and selection testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,31 @@
 #include "rlImGui.h"
 #include <iostream>
 #include <filesystem>
+#include "rlImGuiColors.h"
 
-#define MODEL_FOLDER "models/"
+#define TRAIN_MODELS "models/trains"
+#define FLOOR_MODEL "models/terrain/open.obj"
 
-int main()
+#define screenWidth 1280
+#define screenHeight 720
+
+void setup()
 {
-	const int screenWidth = 1280;
-	const int screenHeight = 720;
-
 	SetWindowState(FLAG_VSYNC_HINT);
 	InitWindow(screenWidth, screenHeight, "Trainering");
 	rlImGuiSetup(true);
+}
+
+enum Debug_Menu
+{
+	DM_MODEL_PREVIEW,
+	DM_TRAIN_ASSEMBLY,
+};
+
+
+int main()
+{
+	setup();
 
 	Camera camera = { 0 };
 	camera.position = Vector3 { 10.0f, 10.0f, 10.0f };
@@ -25,7 +39,7 @@ int main()
 	std::vector<Model> models;
 	std::vector<std::string> modelNames;
 
-	for (const auto& entry : std::filesystem::directory_iterator(MODEL_FOLDER)) {
+	for (const auto& entry : std::filesystem::directory_iterator(TRAIN_MODELS)) {
 		std::string path = entry.path().string();
 		std::string fileName = entry.path().filename().string();
 		if (IsFileExtension(fileName.c_str(), ".obj")) {
@@ -35,17 +49,14 @@ int main()
 		}
 	}
 
+	Model floor = LoadModel(FLOOR_MODEL);
+
 	int modelIndex = 0;
 
 	Vector3 modelPosition = { 0.0f, 0.0f, 0.0f };
 
 	auto& io = ImGui::GetIO();
 
-	enum Debug_Menu
-	{
-		DM_MODEL_PREVIEW,
-		DM_TRAIN_ASSEMBLY,
-	};
 
 	while (!WindowShouldClose())
 	{
@@ -60,7 +71,9 @@ int main()
 
 		static std::vector<part> parts;
 
-		if(!cbOpen && (menu == DM_MODEL_PREVIEW))
+		static bool camera_paused = false;
+
+		if (!camera_paused)
 			UpdateCamera(&camera, CAMERA_ORBITAL);
 
 		BeginDrawing();
@@ -71,6 +84,9 @@ int main()
 
 		BeginMode3D(camera);
 
+		ImGui::Begin("Camera Controls");
+		ImGui::Checkbox("Pause Camera", &camera_paused);
+		ImGui::End();
 
 		if(menu == DM_MODEL_PREVIEW)
 		{
@@ -85,12 +101,51 @@ int main()
 		}
 
 		DrawGrid(20, 1.0f);
+		static float scale = 1.0;
+		static int dim = 1;
+
+		static ImColor color = ImColor(255, 255, 255, 255);
+
+		auto ray = GetMouseRay(GetMousePosition(), camera);
+		for (int x = -dim+1; x < dim; x++)
+		{
+			for (int y = -dim +1; y < dim; y++)
+			{
+				Vector3 pos = { (float)x, 0.0f, (float)y };
+				Vector3 min = { pos.x - 0.5f, pos.y, pos.z - 0.5f };
+				Vector3 max = { pos.x + 0.5f, pos.y + 1.0f, pos.z + 0.5f };
+
+				BoundingBox box = { min, max };
+
+				auto rc = GetRayCollisionBox(ray, box);
+
+				if(rc.hit)
+					DrawModel(floor, { (float)x, 0.0f, (float)y }, scale, BLUE);
+				else
+					DrawModel(floor, { (float)x, 0.0f, (float)y }, scale, rlImGuiColors::Convert(color));
+
+			}
+		}
+
+		static int debugCubeX = 0;
+		static int debugCubeY = 0;
+
+		DrawCube({ (float)debugCubeX, 0.5f, (float)debugCubeY }, 1.0f, 1.0f, 1.0f, RED);
 
 		EndMode3D();
 
-		ImGui::Begin("Trainering");
+		ImGui::Begin("Floor Controls");
+		ImGui::DragFloat("Scale", &scale, 0.1f);
+		ImGui::ColorEdit4("Color", (float*)&color);
+		ImGui::DragInt("Dim", &dim, 1, 1, 10);
 
+		ImGui::Separator();
+		ImGui::Text("Debug Cube");
+		ImGui::DragInt("X", &debugCubeX, 1, -10, 10);
+		ImGui::DragInt("Y", &debugCubeY, 1, -10, 10);
+		ImGui::End();
 
+		ImGui::Begin("Debug Menu");
 
 		if (ImGui::RadioButton("Model Preview", menu == DM_MODEL_PREVIEW)) { menu = DM_MODEL_PREVIEW; } ImGui::SameLine();
 		if (ImGui::RadioButton("Train Assembly", menu == DM_TRAIN_ASSEMBLY)) { menu = DM_TRAIN_ASSEMBLY; }
@@ -154,7 +209,7 @@ int main()
 				}
 
 				std::string df3lbl = "Offset###" + std::to_string(i);
-				ImGui::DragFloat3(df3lbl.c_str(), &parts.at(i).offset.x, 0.1f);
+				ImGui::DragFloat3(df3lbl.c_str(), &parts.at(i).offset.x, 0.2f);
 			}
 		}
 
